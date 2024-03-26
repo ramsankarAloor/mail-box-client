9d1e4a3bf84c69d192febc8c61661ff396e5de88
ramsankarAloor <ramsankar.aloor@gmail.com>
2024-03-26 11:44:20 +0530

    backend needed packages installation

---
+++ Frontend/src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+
+//For react bootstrap
+import "../node_modules/react-bootstrap/dist/react-bootstrap.min";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
